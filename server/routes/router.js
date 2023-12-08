7a5f94ab7c7daa6f1ee79b4bbbbdb81462072aa2
marionafarrate83 <mario.nafarrate@gmail.com>
2023-12-07 22:02:50 -0600

    initial commit

---
+++ server/routes/router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const route = express.Router();
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+//metodos GET
+
+route.get('/',services.homeRoutes);
+
+route.get('/add-user',services.add_user);
+
+route.get('/update-user',services.update_user);
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+//exportar el modulo rutas
+module.exports = route
