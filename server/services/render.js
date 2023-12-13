08c9aab4ad8e3e81192cff36a8a317ab31990319
marionafarrate83 <mario.nafarrate@gmail.com>
2023-12-13 10:47:52 -0600

    ok

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://crudappeq9-f7b9f832fbe4.herokuapp.com/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
         })
