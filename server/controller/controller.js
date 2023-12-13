2edb50d9e5393d7a775c4476d9aed0484e4f95c5
marionafarrate83 <mario.nafarrate@gmail.com>
2023-12-13 12:33:43 -0600

    validation

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -4,7 +4,7 @@ var Userdb = require('../model/dinochar');
 exports.create = (req,res)=>{
     // validate request
     if(!req.body){
-        res.status(400).send({ message : "Content can not be emtpy!"});
+        res.status(400).send({ message : "El contenido no puede estar vacío!"});
         return;
     }
 
