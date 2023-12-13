b5e5f9e65cc98947407fdbc79987a9db3abbd794
marionafarrate83 <mario.nafarrate@gmail.com>
2023-12-13 10:45:52 -0600

    again

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000' || HOST)
+    axios.get('http://localhost:3000/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
         })
