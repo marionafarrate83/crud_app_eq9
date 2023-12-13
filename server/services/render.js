c8780d3d29ac0c864ef62dbae3d2485376182d42
marionafarrate83 <mario.nafarrate@gmail.com>
2023-12-13 10:41:03 -0600

    GLOBAL VAR

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000')
+    axios.get('http://localhost:3000' || HOST)
         .then(function(response){
             res.render('index', { users : response.data });
         })
